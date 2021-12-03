22f95e660e2518bb1073e07c4966265c2f30a52d
Roberto Quintero <langdonhp435@gmail.com>
2021-12-03 14:48:16 -0600

    Google Sign agregado

--- models/usuario.js
+++ models/usuario.js
@@ -20,7 +20,7 @@ const UsuarioSchema = Schema({
   rol: {
     type: String,
     required: true,
-    // enum: ["ADMIN_ROLE", "USER_ROLE"],
+    default: "USER_ROLE",
   },
   estado: {
     type: Boolean,
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { login } = require("../controllers/auth");
+const { login, googleSignin } = require("../controllers/auth");
 const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
@@ -11,6 +11,13 @@ router.post(
   check("password", "La contraseña es obligatoria").not().isEmpty(),
   validarCampos,
   login
+);
+
+router.post(
+  "/google",
+  check("id_token", "id_token de google es necesario").not().isEmpty(),
+  validarCampos,
+  googleSignin
 );
 
 module.exports = router;
